3b3c09231e2a40c276fc1eb8d951f06a5c192e1e
JorgeOchoaDev <jl91ob@protonmail.com>
2020-01-20 14:41:51 -0600

    ver 1.0

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+*Yo tengo un servicio de valet parking.
+*En este servicio yo dejo las llaves de mis clientes dentro de una caja que tiene compartimientos.
+*Para tener seguimiento de el carro al que pertenece cada llave en mi caja, declaré un array llamado cajaDeLlaves.
+*Necesito una función entraCarro(carro,lugar) a la que le pase 2 datos: El color y modelo del carro, y el lugar de mi caja en donde puse las llaves
+* Ej: entraCarro("Bocho gris", 0) hace que mi array llaves = ["Bocho gris","otro carro","oootro carro"]
+*Y necesito otra función saleCarro() que reciba el dato del lugar dentro de mi caja y me devuelva el color y modelo del carro al que pertenece esa llave
+* Ej: saleCarro(0) me devuelve el valor "Bocho gris"
+*/
+
+var llaves = []
+
+function entraCarro () {
+
+}
+
+function saleCarro () {
+
+}

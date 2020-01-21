95836051d80ae9920cb9b186efd6a174a99249e0
JorgeOchoaDev <jl91ob@protonmail.com>
2020-01-20 19:02:16 -0600

    complication update

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 *En este servicio yo dejo las llaves de mis clientes dentro de una caja que tiene compartimientos.
 *Para tener seguimiento de el carro al que pertenece cada llave en mi caja, declaré un array llamado cajaDeLlaves.
 *Necesito una función entraCarro(carro,lugar) a la que le pase 2 datos: El color y modelo del carro, y el lugar de mi caja en donde puse las llaves
-* Ej: entraCarro("Bocho gris", 0) hace que mi array llaves = ["Bocho gris","otro carro","oootro carro"]
+* Ej: entraCarro("Bocho gris", 0) hace que mi array llaves = ["bocho gris","otro carro","oootro carro"]
 *Y necesito otra función saleCarro() que reciba el dato del lugar dentro de mi caja y me devuelva el color y modelo del carro al que pertenece esa llave
-* Ej: saleCarro(0) me devuelve el valor "Bocho gris"
+* Ej: saleCarro(0) me devuelve el valor "bocho gris"
+* Complicación: En vez de pasarle un número a saleCarro() y que me devuelva la descripción, quiero pasarle la descripción del carro y que me devuelva el lugar de la llave.
+* Ej: saleCarro("bocho gris") me devuelve un valor con su posición en mi array cajaDeLlaves
 */
 
 var cajDeLlaves = []
